59def18ec4237aa6ca83be96027fc0cace489721
LENOVO <LENOVO@DESKTOP-CSHPDLO>
2024-05-28 22:28:46 +0300

    Firstcommit

---
+++ Practice/src/test/java/CountStr.java
@@ -0,0 +1,31 @@
+
+public class CountStr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  String str1="aaaabbbbcddfgg";
+  String str2="";
+  
+  char ch[]=new char[str1.length()];
+  int count=0;
+  
+  
+  for(int i=0;i<str1.length();i++)
+  {
+	  count=0;
+	  for(int j=0;j<str1.length();j++)
+	  {  
+		  if(str1.charAt(i)==str1.charAt(j))
+		  {
+			 count++; 
+		  }
+		  
+		   ch[i]=str1.charAt(i);
+		 // str2=str2+str1.charAt(i);
+  }
+	  System.out.println(ch[i]+" Given String :"+count);
+	}
+  
+	}
+
+}
---
+++ Practice/src/test/java/OrderOfExecution.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.Test;
+
+public class OrderOfExecution {
+	
+	
+
+	@Test(invocationCount=3)
+	public void Method1() {
+	
+	System.out.println("Method1 execution");	
+	
+	}
+	
+	@Test(priority=1)
+	public void Method2() {
+		
+		System.out.println("Method2 execution");	
+	}
+	
+	@Test(priority=-10)
+	public void Method3() {
+		
+		System.out.println("Method3 execution");	
+	}
+
+}
